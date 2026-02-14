a7bfdaeaf298a33fb2c47418865adb269d1c46a6
vixendi <vixendi@gmail.com>
2026-02-14 20:19:18 +0100

    refactor(glue): configure crawler targets for customers/sales/user_profiles

--- assesssment_work/data-platform/infra/modules/glue/main.tf
+++ assesssment_work/data-platform/infra/modules/glue/main.tf
@@ -1,26 +1,28 @@
 locals {
   glue_db_name     = "${var.project_name}_glue_db"
   raw_crawler_name = "${var.project_name}-${var.env}-raw-crawler"
-  raw_s3_path      = "s3://${var.datalake_bucket_name}/raw/"
+
+  s3_customers_path    = "s3://${var.datalake_bucket_name}/raw/customers/"
+  s3_sales_path        = "s3://${var.datalake_bucket_name}/raw/sales/"
+  s3_user_profiles_path = "s3://${var.datalake_bucket_name}/raw/user_profiles/"
 }
 
 resource "aws_glue_catalog_database" "this" {
   name = local.glue_db_name
 }
 
+# Single crawler with 3 S3 targets (customers, sales, user_profiles)
 resource "aws_glue_crawler" "raw" {
   name          = local.raw_crawler_name
   role          = var.glue_role_arn
   database_name = aws_glue_catalog_database.this.name
 
-  # Всі raw-таблиці будуть з префіксом raw_
-  table_prefix  = "raw_"
+  table_prefix = "raw_"
 
-  s3_target {
-    path = local.raw_s3_path
-  }
+  s3_target { path = local.s3_customers_path }
+  s3_target { path = local.s3_sales_path }
+  s3_target { path = local.s3_user_profiles_path }
 
-  # Мінімум “магії” та мінімум зайвих перезаписів
   recrawl_policy {
     recrawl_behavior = "CRAWL_EVERYTHING"
   }
