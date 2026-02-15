4934b920b722297db47964d936be9361dc50942d
vixendi <vixendi@gmail.com>
2026-02-15 17:21:10 +0100

    fix(outputs): align MWAA outputs with module interface

--- assesssment_work/data-platform/infra/envs/dev/outputs.tf
+++ assesssment_work/data-platform/infra/envs/dev/outputs.tf
@@ -22,3 +22,12 @@ output "GlueJobSalesRawToBronze" { value = module.glue.job_sales_raw_to_bronze }
 output "GlueJobSalesBronzeToSilver" { value = module.glue.job_sales_bronze_to_silver }
 output "GlueJobCustomersRawToSilver" { value = module.glue.job_customers_raw_to_silver }
 output "GlueJobUserProfilesRawToSilver" { value = module.glue.job_profiles_raw_to_silver }
+
+output "RedshiftWorkgroupEndpoint" { value = module.redshift.endpoint }
+output "RedshiftSecretArn" { value = module.redshift.secret_arn }
+output "RedshiftNamespaceName" { value = module.redshift.namespace_name }
+output "RedshiftWorkgroupName" { value = module.redshift.workgroup_name }
+
+output "MWAAName" { value = module.mwaa.name }
+output "MWAAWebserverUrl" { value = module.mwaa.webserver_url }
+output "MWAAExecRoleArn" { value = module.mwaa.exec_role_arn }
